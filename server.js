2dc0c8ffe178b65bc4c384b21fda402c1a2b8b4b
jaypee-0 <eyibrajohnpaul@gmail.com>
2022-04-26 15:54:34 +0100

    Addedd '/sys' path

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const http = require('http')
 const fs = require('fs')
+const os = require('os')
+const json = [
+    os.hostname(),
+    os.platform(),
+    os.arch(), 
+    os.cpus().length, 
+    os.uptime(),
+    os.networkInterfaces()
+    ];
+const osJSON = JSON.stringify(json);
 
 const host = '127.0.0.1'
 const port = 5000
@@ -14,14 +24,18 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, { 'Content-Type': 'text/html' })
         const about = fs.readFileSync('pages/about.html')
         res.end(about)
-    } else {
+    } else if (urlPath === '/sys') {
+        fs.writeFileSync('./osinfo.json', osJSON, () => {
+            return;            
+        })
+    }
+     else {
         res.statusCode = 404;
         res.setHeader('Content-Type', 'text/html');
         const error = fs.readFileSync('pages/404.html')
         res.end(error);
     }
 })
-
 server.listen(port, host, () => {
     console.log(`Server started at ${host}:${port}`)
 })
