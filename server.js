81203e9604d1668edf1a8d9ebfd891b84e2d8224
jaypee-0 <eyibrajohnpaul@gmail.com>
2022-04-26 02:31:22 +0100

    Added 404 - Page not found

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
-// const http = require('http')
-// const fs = require('fs')
+const http = require('http')
+const fs = require('fs')
 
-// const host = '127.0.0.1'
-// const port = 5000
+const host = '127.0.0.1'
+const port = 5000
 
-// const server = http.createServer((req, res) => {
-//     const urlPath = req.url
-//     if (urlPath === '/') {
-//         res.writeHead(200, { 'Content-Type': 'text/plain' })
-//         res.end('Home page')
-//     } else if (urlPath === '/about') {
-//         res.writeHead(200, { 'Content-Type': 'text/html' })
-//         const about = fs.readFileSync('pages/about.html')
-//         res.end(about)
+const server = http.createServer((req, res) => {
+    const urlPath = req.url
+    if (urlPath === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' })
+        res.end('Home page')
+    } else if (urlPath === '/about') {
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        const about = fs.readFileSync('pages/about.html')
+        res.end(about)
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        const error = fs.readFileSync('pages/404.html')
+        res.end(error);
+    }
+})
 
-//     }
-
-// })
-
-// server.listen(port, host, () => {
-//     console.log(`sever started at port: ${port}`)
-// })
+server.listen(port, host, () => {
+    console.log(`Server started at ${host}:${port}`)
+})
